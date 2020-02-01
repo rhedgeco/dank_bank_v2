3a0c957e0de063ed6175ee530fa6f030be3356a4
rhedgeco <rhedgeco@gmail.com>
2020-01-31 17:28:20 -0800

    better placeholder styling lol

--- frontend/data/js/google-oauth.js
+++ frontend/data/js/google-oauth.js
@@ -8,7 +8,10 @@ function onSignIn(googleUser) {
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
 
     document.getElementById('main-text').innerText = 'Welcome, ' + profile.getName();
-    document.getElementById('login-photo').innerHTML = '<img src="'+profile.getImageUrl()+'" alt="profile-photo">';
+    document.getElementById('login-photo').innerHTML = '' +
+        '<img src="'+profile.getImageUrl()+'" ' +
+        'alt="profile-photo" ' +
+        'style="width: 100px; height: 100px">';
 
     send_token(id_token);
 }
