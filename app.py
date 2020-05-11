85dede095ff8ee0661c3aab6c352ed2d4ec4bc0f
rhedgeco <rhedgeco@gmail.com>
2020-05-11 13:20:12 -0700

    modified the updater to be used as an endpoint

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from general_falcon_webserver import WebApp, SqliteDatabase
 from backend.database_manager import DatabaseManager
 from backend.endpoints.google_oauth import GoogleOauth
 from backend.endpoints.groups import Groups
+from backend.endpoints.self_updater import Updater
 from backend.endpoints.transactions import Transactions
 from backend.endpoints.users import Users
 
@@ -41,6 +42,9 @@ def configure_app():
 
     trans = Transactions(manager)
     app.add_route('transactions', trans)
+
+    updater = Updater()
+    app.add_route('git_updater', updater)
 
     return app
 
--- self_updater.py
+++
@@ -1,9 +0,0 @@
-import git
-from pathlib import Path
-
-repo = git.Repo(Path('.').absolute())
-origin = repo.remotes.origin
-origin.pull()
-
-for submodule in repo.submodules:
-    submodule.update(init=True)
