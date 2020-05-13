19c6acd0e8ba963fc98b8bfe0ccc8eec181a4688
rhedgeco <rhedgeco@gmail.com>
2020-05-12 18:04:14 -0700

    added navbar to other pages

--- frontend/data/js/create_group.js
+++ frontend/data/js/create_group.js
@@ -1,19 +1,3 @@
-function getCookie(cname) {
-  var name = cname + '=';
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return 0;
-}
-
 // Get the modal
 var modal = document.getElementById('createModal');
 
@@ -25,32 +9,32 @@ var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
-  modal.style.display = 'block';
-  console.log(document.getElementById('group_name').value);
+    modal.style.display = 'block';
+    console.log(document.getElementById('group_name').value);
 };
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = 'none';
+    modal.style.display = 'none';
 };
 
 //API call
 var btn2 = document.getElementById('create_form');
 
 btn2.onclick = function () {
-  let xhr = new XMLHttpRequest();
-  xhr.open(
-    'POST',
-    'api/groups?session=' +
-      getCookie('session_id') +
-      '&group_name=' +
-      document.getElementById('group_name').value
-  );
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function () {
-    if (xhr.status === 200) {
-      console.log('group successfully created');
-    }
-  };
-  xhr.send();
+    let xhr = new XMLHttpRequest();
+    xhr.open(
+        'POST',
+        'api/groups?session=' +
+        getCookie('session_id') +
+        '&group_name=' +
+        document.getElementById('group_name').value
+    );
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            console.log('group successfully created');
+        }
+    };
+    xhr.send();
 };
--- frontend/data/js/home_init.js
+++ frontend/data/js/home_init.js
@@ -1,80 +1,22 @@
-let group_list = document.getElementById('groups'); //store reference to the list of groups
-
-function getCookie(cname) {
-  var name = cname + '=';
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) === ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) === 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return 0;
-}
-
-function init_home() {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'api/users?session=' + getCookie('session_id'));
-  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onload = function () {
-    if (xhr.status !== 200) {
-      window.location = '/';
-    }
-
-    if (xhr.status === 200) {
-      var json = JSON.parse(xhr.responseText);
-      console.log(json);
-      document.getElementById('name').innerText = json['nickname']; //retrieve and display nick name
-
-      // display user photo
-      document.getElementById('login-photo').src = json['photo'];
-
-      let groups = json['groups']; //retreive groups from JSON
-
-      //go through list of groups and create list elements
-      for (var i = 0; i < groups.length; i++) {
-        let group = document.createElement('li');
-        group.id = Object.keys(groups[i]); //set id to the same group id from the db
-        group.appendChild(document.createTextNode(Object.values(groups[i])[0])); //get group name from the object
-        group_list.appendChild(group);
-      }
-
-      //add event listener to each group on the list
-      for (let i = 0; i < group_list.childElementCount; i++) {
-        let list_item = group_list.childNodes[i];
-
-        //make API call to retrieve group info
-        list_item.addEventListener('click', () => {
-          let xhr = new XMLHttpRequest();
-          xhr.open(
-            'GET',
-            'api/groups?session=' +
-              getCookie('session_id') +
-              '&group_id=' +
-              list_item.id
-          );
-          xhr.setRequestHeader(
-            'Content-Type',
-            'application/x-www-form-urlencoded'
-          );
-          xhr.onload = function () {
-            if (xhr.status === 200) {
-              var groupInfo = JSON.parse(xhr.responseText);
-              console.log(groupInfo);
-            }
-          };
-          xhr.send();
-        });
-      }
-    }
-  };
-  xhr.send();
-}
-
-window.onload = function () {
-  init_home();
-};
+// function init_home() {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'api/users?session=' + getCookie('session_id'));
+//     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+//     xhr.onload = function () {
+//         if (xhr.status !== 200) {
+//             window.location = '/';
+//         }
+//
+//         if (xhr.status === 200) {
+//             var json = JSON.parse(xhr.responseText);
+//             console.log(json);
+//             document.getElementById('main-text').innerText = json['nickname']; //retrieve and display nick name
+//             document.getElementById('login-photo').src = json['photo'];
+//         }
+//     };
+//     xhr.send();
+// }
+//
+// window.onload = function () {
+//     init_home();
+// };
