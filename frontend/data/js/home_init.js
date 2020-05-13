3fb45b548d272bfb6b38a8610c06f217c313e5eb
rhedgeco <rhedgeco@gmail.com>
2020-05-12 20:54:38 -0700

    updated navbar to work better on mobile

--- frontend/data/js/home_init.js
+++
@@ -1,8 +0,0 @@
-import { loadUser, configureGroups } from './load_user.js';
-
-async function init_home() {
-  let user = await loadUser();
-  configureGroups();
-}
-
-$(document).ready(init_home());
