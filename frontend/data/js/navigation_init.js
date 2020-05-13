e5aa03624cf6e8abd326960bfb66992cd703251e
rhedgeco <rhedgeco@gmail.com>
2020-05-12 20:05:05 -0700

    cleaned nav init

--- frontend/data/js/navigation_init.js
+++ frontend/data/js/navigation_init.js
@@ -14,6 +14,20 @@ function populate_names(names, name) {
     }
 }
 
+function populate_groups(group_list, groups) {
+    //go through list of groups and create list elements
+    for (var i = 0; i < groups.length; i++) {
+        let group = document.createElement('li');
+        console.log(group.id);
+        group.id = Object.keys(groups[i]); //set id to the same group id from the db
+        group.appendChild(document.createTextNode(Object.values(groups[i])[0])); //get group name from the object
+        group_list.appendChild(group);
+        group.onclick = function () {
+            goto_group_page(this.id);
+        }
+    }
+}
+
 function init_navigation() {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', 'api/users?session=' + getCookie('session_id'));
@@ -25,30 +39,20 @@ function init_navigation() {
             let json = JSON.parse(xhr.responseText);
             let names = document.getElementsByClassName('user-name');
             let photos = document.getElementsByClassName('login-photo');
+            let group_list = document.getElementById('groups'); //store reference to the list of groups
             populate_names(names, json['nickname']);
             populate_photos(photos, json['photo']);
-            let groups = json['groups'];
-            //go through list of groups and create list elements
-            let group_list = document.getElementById('groups'); //store reference to the list of groups
-            for (var i = 0; i < groups.length; i++) {
-                let group = document.createElement('li');
-                group.id = Object.keys(groups[i]); //set id to the same group id from the db
-                group.appendChild(document.createTextNode(Object.values(groups[i])[0])); //get group name from the object
-                group_list.appendChild(group);
-                group.onclick = function () {
-                    goto_group_page(this.id);
-                }
-            }
+            populate_groups(group_list, json['groups'])
+
         }
     };
     xhr.send();
 }
 
-window.onload = function () {
-    init_navigation();
-};
 
 function sign_out() {
     document.cookie = 'session_id=0';
     window.location = '/';
 }
+
+$(document).ready(init_navigation());
