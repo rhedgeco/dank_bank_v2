7aa50bb711bc0ad1e53a61871357f704bdb9fc02
Danny Giap <dannygiap@gmail.com>
2020-05-13 18:23:06 -0700

    create group redirect

--- frontend/data/js/create_group.js
+++ frontend/data/js/create_group.js
@@ -6,32 +6,31 @@ var span = document.getElementsByClassName('close')[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-    modal.style.display = 'none';
+  modal.style.display = 'none';
 };
 
 //API call
 var btn2 = document.getElementById('create_form');
 
 btn2.onclick = function () {
-    let xhr = new XMLHttpRequest();
-    xhr.open(
-        'POST',
-        'api/groups?session=' +
-        getCookie('session_id') +
-        '&group_name=' +
-        document.getElementById('group_name').value
-    );
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = function () {
-        if (xhr.status === 200) {
-            console.log('group successfully created');
-            location.reload();
-        }
-    };
-    xhr.send();
+  let xhr = new XMLHttpRequest();
+  xhr.open(
+    'POST',
+    'api/groups?session=' +
+      getCookie('session_id') +
+      '&group_name=' +
+      document.getElementById('group_name').value
+  );
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      window.location = 'group.html?group_id=' + xhr.responseText;
+    }
+  };
+  xhr.send();
 };
 
 function create_btn_clicked() {
-    modal.style.display = 'block';
-    console.log(document.getElementById('group_name').value);
+  modal.style.display = 'block';
+  console.log(document.getElementById('group_name').value);
 }
